58e1292c9631b3b51b59f60773ac6f9b6dedd1db
John Doe <guyg09871@gmail.com>
2024-12-24 01:34:50 +0800

    Preview added

--- Problem5/keeper.go
+++ Problem5/keeper.go
@@ -1,26 +1,26 @@
 package keeper
 
 import (
-	"x/crude/types"
+	"x/crude/types" // your custom module types
 
-	"github.com/cosmos/cosmos-sdk/types"
+	sdk "github.com/cosmos/cosmos-sdk/types" // Cosmos SDK types
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 type Keeper struct {
-	storeKey types.StoreKey
-	cdc      types.Codec
+	storeKey sdk.StoreKey
+	cdc      sdk.Codec
 }
 
 // CreateResource creates a new resource in the store
-func (k Keeper) CreateResource(ctx types.Context, resource types.Resource) error {
+func (k Keeper) CreateResource(ctx sdk.Context, resource types.Resource) error {
 	store := ctx.KVStore(k.storeKey)
 	store.Set([]byte(resource.Id), k.cdc.MustMarshalBinaryBare(&resource))
 	return nil
 }
 
 // GetResource retrieves a resource from the blockchain by ID
-func (k Keeper) GetResource(ctx types.Context, id string) (types.Resource, error) {
+func (k Keeper) GetResource(ctx sdk.Context, id string) (types.Resource, error) {
 	store := ctx.KVStore(k.storeKey)
 	data := store.Get([]byte(id))
 	if data == nil {
@@ -33,14 +33,14 @@ func (k Keeper) GetResource(ctx types.Context, id string) (types.Resource, error
 }
 
 // UpdateResource updates a resource in the store
-func (k Keeper) UpdateResource(ctx types.Context, resource types.Resource) error {
+func (k Keeper) UpdateResource(ctx sdk.Context, resource types.Resource) error {
 	store := ctx.KVStore(k.storeKey)
 	store.Set([]byte(resource.Id), k.cdc.MustMarshalBinaryBare(&resource))
 	return nil
 }
 
 // DeleteResource deletes a resource from the store
-func (k Keeper) DeleteResource(ctx types.Context, id string) error {
+func (k Keeper) DeleteResource(ctx sdk.Context, id string) error {
 	store := ctx.KVStore(k.storeKey)
 	store.Delete([]byte(id))
 	return nil
